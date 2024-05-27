82e0069435170951f767f62c4ec7301898902a96
Nithin <nithin@learneon.com>
2024-05-27 16:46:37 +0530

    save changes

--- chat/static/js/ai_process.js
+++ chat/static/js/ai_process.js
@@ -2,3 +2,9 @@
 function aiChatPage(){
     window.location.href = '/ai_chatpage/';
 }
+
+function aiProcessLatestRecordedFile(){
+    const ai_process_latestfile = document.getElementById('ai-process-latestfile');
+    
+}
+
